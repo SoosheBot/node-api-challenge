3389f418acf9828d1e9c229594e6bd4130ea1893
sooshe <sooshe@sooshebot.com>
2020-02-14 10:29:09 -0600

    complete scaffold for server.js file

--- server.js
+++ server.js
@@ -1,4 +1,17 @@
 const express = require('express');
 const server = express;
+const helmet = require('helmet');
 const cors = require('cors');
 
+
+
+server.use(helmet());
+server.use(express.json());
+server.use(cors);
+
+//init test of server on insomnia
+server.get("/", (req,res) => {
+    res.status(200).json({message:"Server is running."})
+});
+
+module.exports = server;
