374c1868c0c62b26fdace1fad415405e404a2ce4
sooshe <sooshe@sooshebot.com>
2020-02-14 10:54:46 -0600

    projRouter GET req success--create GET req in actRouter

--- data/routers/actionRouter.js
+++ data/routers/actionRouter.js
@@ -7,4 +7,4 @@ const router = express.Router();
 
 router.use(express.json());
 
-module.exports = actionRouter;
+module.exports = router;
--- data/routers/projectRouter.js
+++ data/routers/projectRouter.js
@@ -22,4 +22,4 @@ router.get("/", (req,res) => {
     });
 });
 
-module.exports = projectRouter;
+module.exports = router;
