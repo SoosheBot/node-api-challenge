26b3f27c6f888259cae78457e4c5780dca41e133
sooshe <sooshe@sooshebot.com>
2020-02-14 10:43:02 -0600

    debug server file -- server running

--- server.js
+++ server.js
@@ -1,16 +1,31 @@
-const express = require('express');
-const helmet = require('helmet');
-const cors = require('cors');
+const express = require("express");
+const server = express();
 
-const server = express;
+const helmet = require("helmet");
+const cors = require("cors");
 
-server.use(helmet());
+const projectRouter = require("./data/routers/projectRouter");
+const actionRouter = require("./data/routers/actionRouter");
+
 server.use(express.json());
-server.use(cors);
+server.use(helmet());
+server.use(cors());
+
+// server.use("/api/projects", logger, projectRouter);
+// server.use("/api/projects", logger, actionRouter);
 
 //init test of server on insomnia
-server.get("/", (req,res) => {
-    res.status(200).json({message:"Server is running."})
+server.get("/", (req, res) => {
+  res.status(200).json({ message: "Server is running." });
 });
+
+function logger(req, res, next) {
+  console.log(
+    `[${new Date().toISOString()}] ${req.method} to ${req.url} from ${req.get(
+      "host"
+    )}`
+  );
+  next();
+}
 
 module.exports = server;
