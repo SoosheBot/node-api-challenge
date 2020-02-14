abf21a17c575bf0cf996c6c201c06870c0c4fd4b
sooshe <sooshe@sooshebot.com>
2020-02-14 10:52:56 -0600

    test GET req in projRouter file

--- data/routers/projectRouter.js
+++ data/routers/projectRouter.js
@@ -6,4 +6,20 @@ const router = express.Router();
 
 router.use(express.json());
 
+//GET projects
+router.get("/", (req,res) => {
+    dbP.get()
+    .then(projects => {
+        if (projects) {
+            res.status(200).json(projects);
+        } else {
+            res.status(404).json({ error: "Projects content not found."})
+        };
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({errorMessage: "Could not GET projects."});
+    });
+});
+
 module.exports = projectRouter;
