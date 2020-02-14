513f2728edad79f64e855474712d7fcefd1d7aeb
sooshe <sooshe@sooshebot.com>
2020-02-14 10:46:25 -0600

    init setup actionRouter file

---
+++ data/routers/actionRouter.js
@@ -0,0 +1,8 @@
+const express = require("express");
+
+const dbA = require("../helpers/actionModel");
+const dbP = require("../helpers/projectModel");
+
+const router = express.Router();
+
+router.use(express.json());
